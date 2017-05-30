9bb2e905efae3cc3ccc95d6e1710c4c768051fae
Bruno <bruno@bettacom.com.br>
2017-05-30 20:13:58 -0300

    Adding rest of the activitys
    mounting navigation for all activity, and layouts

--- app/src/main/java/atmempresaconsultoria/cursoandroid/com/atmempresaconsultoria/MainActivity.java
+++ app/src/main/java/atmempresaconsultoria/cursoandroid/com/atmempresaconsultoria/MainActivity.java
@@ -1,14 +1,52 @@
 package atmempresaconsultoria.cursoandroid.com.atmempresaconsultoria;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+
+    private ImageView btnEmpresa;
+    private ImageView btnServico;
+    private ImageView btnCliente;
+    private ImageView btnContato;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnEmpresa = (ImageView) findViewById(R.id.btnEmpresa);
+        btnEmpresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EmpresaActivity.class));
+            }
+        });
+        btnServico = (ImageView) findViewById(R.id.btnServicos);
+        btnServico.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ServicoActivity.class));
+            }
+        });
+        btnCliente = (ImageView) findViewById(R.id.btnCliente);
+        btnCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ClienteActivity.class));
+            }
+        });
+        btnContato = (ImageView) findViewById(R.id.btnContato);
+        btnContato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ContatoActivity.class));
+            }
+        });
+
     }
 }
